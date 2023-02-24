d41e7de0f994057844d284f2330a205608a2200b
volkan <volkandogru11@gmail.com>
2023-02-24 10:39:07 +0300

    copy button edited

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RgbToHex from "./RgbToHex";
 function App() {
   return (
     <div className="App">
-      <h1>Colorvolki</h1>
+      <h1>VolkiColor</h1>
       <div className="card">
         <Color />
         <RgbToHex />
